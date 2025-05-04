873b5e4a3970ef94e84bca0e46b5c3a6bf261946
Colin D Murphy <cdmurph32@gmail.com>
2025-05-04 17:11:54 -0400

    test complex scenarios where dependent crates in same repo are updated (#2196)
    Co-authored-by: Marco Ieni <11428655+marcoieni@users.noreply.github.com>

--- crates/release_plz_core/src/command/update/updater.rs
+++ crates/release_plz_core/src/command/update/updater.rs
@@ -279,7 +279,7 @@ impl Updater<'_> {
         Ok(packages_diffs)
     }
 
-    /// Return the update to apply to the packages that depend on the `changed_packages`.
+    /// Return the update to apply to the packages that depend on the `initial_changed_packages`.
     ///
     /// ## Args
     ///
@@ -287,7 +287,7 @@ impl Updater<'_> {
     ///   We update them if they depend on any of the `changed_packages`.
     ///   If they don't depend on any of the `changed_packages`, they are not updated
     ///   because they don't contain any new commits.
-    /// - `changed_packages`: The packages that have changed (i.e. contains commits).
+    /// - `initial_changed_packages`: The packages that have changed (i.e. contains commits).
     fn dependent_packages_update(
         &self,
         packages_to_check_for_deps: &[&Package],
