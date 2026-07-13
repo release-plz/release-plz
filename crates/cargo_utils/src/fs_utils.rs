abcf71fc69136814729871988b50bf3d937b82ed
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2026-07-13 22:29:17 +0200

    fix clippy lint of rust 1.97 (#2932)

--- crates/cargo_utils/src/fs_utils.rs
+++ crates/cargo_utils/src/fs_utils.rs
@@ -5,6 +5,6 @@ use cargo_metadata::camino::Utf8PathBuf;
 pub fn to_utf8_pathbuf(path: PathBuf) -> anyhow::Result<Utf8PathBuf> {
     match Utf8PathBuf::from_path_buf(path) {
         Ok(p) => Ok(p),
-        Err(path) => Err(anyhow::anyhow!("cannot convert {:?} to Utf8Path", &path)),
+        Err(path) => Err(anyhow::anyhow!("cannot convert {path:?} to Utf8Path")),
     }
 }
