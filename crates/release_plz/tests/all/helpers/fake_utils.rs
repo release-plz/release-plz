02301a67cddaacd5405c8117b94a51fcaa33e29b
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-09-08 22:12:08 -0400

    remove the fake crate

--- crates/release_plz/tests/all/helpers/fake_utils.rs
+++ crates/release_plz/tests/all/helpers/fake_utils.rs
@@ -1,7 +1,5 @@
-use fake::{Fake, StringFaker};
+use std::{collections::hash_map::RandomState, hash::BuildHasher};
 
 pub fn fake_id() -> String {
-    const LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
-    let f = StringFaker::with(Vec::from(LETTERS), 8);
-    f.fake()
+    format!("test{:016x}", RandomState::new().hash_one(()))
 }
